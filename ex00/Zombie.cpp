c2652165ab599e0dd9dddd7f8a4ce59f7e814dd4
lvarela <lvarela@student.42madrid.com>
2022-05-20 15:24:16 +0200

    now show a message when destructed

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,25 +6,35 @@
 /*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 15:15:38 by lvarela           #+#    #+#             */
-/*   Updated: 2022/05/20 09:44:12 by lvarela          ###   ########.fr       */
+/*   Updated: 2022/05/20 15:23:27 by lvarela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+// Constructor and destructor
+
 Zombie::Zombie( void ) { return ; }
-Zombie::~Zombie( void ) { return ; }
+Zombie::~Zombie( void ) {
+	std::cout << this->_name << " has been destroyed" << std::endl;
+	return ;
+}
+
+// Set variables functions
 
 void		Zombie::setName( std::string name ) {
 	this->_name = name;
 }
 
+// Get variables functions
+
 std::string	Zombie::getName( void ) {
 	return (this->_name);
 }
 
-// <name> BraiiiiiiinnnzzzZ...
+//	Other functions
 
+// <name> BraiiiiiiinnnzzzZ...
 void		Zombie::announce( void ){
 	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
