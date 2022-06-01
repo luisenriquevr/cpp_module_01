5bd90f4d719066cf40f11386e2cb255639a2cd5d
luisenriquevr <trabajo.luisen@gmail.com>
2022-06-01 16:14:29 +0200

    cpp01-ex01 done

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:15:38 by lvarela           #+#    #+#             */
+/*   Updated: 2022/06/01 10:43:28 by lvarela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+/* Constructor and destructor */
+
+Zombie::Zombie( void ) { }
+
+Zombie::Zombie( std::string name ) {
+	this->_name = name;
+}
+
+Zombie::~Zombie( void ) {
+	std::cout << "Zombie " << this->_name << " has been destroyed" << std::endl;
+}
+
+/* Setter functions */
+
+void		Zombie::setName( std::string name ) {
+	this->_name = name;
+}
+
+/* Getter functions */
+
+std::string	Zombie::getName( void ) const {
+	return (this->_name);
+}
+
+/*	Other functions */
+
+// <name> BraiiiiiiinnnzzzZ...
+void		Zombie::_announce( void ) {
+	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex01/Zombie.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:15:41 by lvarela           #+#    #+#             */
+/*   Updated: 2022/06/01 16:01:43 by lvarela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+/* Zombie class */
+
+class	Zombie {
+	private:
+		std::string		_name;
+	public:
+		Zombie(void);
+		Zombie(std::string);
+		~Zombie(void);
+		void			setName(std::string);
+		std::string		getName(void) const;
+		void			_announce(void);
+		
+};
+
+/* Functions out of the class */
+
+Zombie	*newZombie( std::string name );
+void	randomChump( std::string name );
+Zombie* zombieHorde( int N, std::string name );
+
+#endif
